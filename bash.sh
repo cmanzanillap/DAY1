d759696f4d1a026e838234bd3eee557446c2ebb5
Coralia Manzanilla Pech <coralia.manzanillapech@wur.nl>
2025-02-18 13:02:12 +0100

    Add files via upload

--- bash.sh
+++ bash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-module load groups
-module --ignore_cache load WUR/ABGC/SnifferAnalyzer
+ml use /lustre/shared/Courses/RELIVESTOCK2025/modules
+
+module load SnifferAnalyzer
 
 sniffer_analyzer herd_robot101.csv herd_sniffer101_Loggy_42s_FD1.txt 42
